1bc2d21fb9035b976d934bc6a349290f7d78621b
pecology <dummy>
2019-11-04 15:23:03 +0900

    ViewModelを作成し、それを表示するようにした

--- Bpms/Controllers/DailyAttendancesController.cs
+++ Bpms/Controllers/DailyAttendancesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bpms.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bpms.Controllers
@@ -10,7 +11,13 @@ namespace Bpms.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var items = new List<DailyAttendanceViewModel>
+            {
+                new DailyAttendanceViewModel {Day = "1", DayOfWeek = "金", StartTime = "9:00", EndTime = "18:00", ActualWorkHours = "8:00", Category = ""},
+                new DailyAttendanceViewModel {Day = "2", DayOfWeek = "土", StartTime = "9:00", EndTime = "18:00", ActualWorkHours = "8:00", Category = ""},
+                new DailyAttendanceViewModel {Day = "3", DayOfWeek = "日", StartTime = "9:00", EndTime = "18:00", ActualWorkHours = "8:00", Category = ""}
+            };
+            return View(items);
         }
     }
 }
---
+++ Bpms/Models/ViewModels/DailyAttendanceViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bpms.Models.ViewModels
+{
+    public class DailyAttendanceViewModel
+    {
+        public string Day { get; set; }
+        public string DayOfWeek { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string ActualWorkHours { get; set; }
+        public string Category { get; set; }
+    }
+}
