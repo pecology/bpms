e5e852e3994694b8e40d534dc39a06ca1e59bce9
pecology <dummy>
2019-11-07 23:21:27 +0900

    勤怠登録画面クライアントサイド実装

--- Bpms/wwwroot/js/DailyAttendance/edit.js
+++ Bpms/wwwroot/js/DailyAttendance/edit.js
@@ -1,1 +1,60 @@
-﻿
+﻿window.onload = () => {
+  document.querySelector("#detail-add-button").onclick = () => {
+    const lastTr = document.querySelector("#detail-table > tbody > tr:nth-last-child(1)");
+
+    const lastDetailNo = lastTr.querySelector(".detail-no").textContent;
+    const addDetailNo = parseInt(lastDetailNo) + 1;
+
+    const lastDetailEndHour = lastTr.querySelector(".end-hour-text").value;
+    const addDetailStartHour = lastDetailEndHour;
+
+    const lastDetailEndMinute = lastTr.querySelector(".end-minute-text").value;
+    const addDetailStartMinute = lastDetailEndMinute;
+
+    const addDetailHtmlString = 
+      `
+          <tr>
+            <td class="detail-no">${addDetailNo}</td>
+            <td>
+              <input type="text" class="start-hour-text" value="${addDetailStartHour}" />時
+              <input type="text" class="start-minute-text" value="${addDetailStartMinute}" />分～
+              <input type="text" class="end-hour-text" value="" />時
+              <input type="text" class="end-minute-text" value="" />分
+            </td>
+            <td><textarea type="text" class="description"></textarea></td>
+            <td><button class="detail-delete-button">削除</button></td>
+          </tr>
+      `;
+
+    const detailTbody = document.querySelector("#detail-table > tbody");
+    detailTbody.insertAdjacentHTML('beforeend', addDetailHtmlString);
+  }
+
+  document.addEventListener('click', e => {
+    const target = e.target || e.srcElement;
+    if (target.getAttribute("class") == 'detail-delete-button') {
+      const parentTr = target.closest("tr");
+
+      const laterTrList = getNextAll(parentTr);
+      laterTrList.forEach(tr => {
+        const noStr = tr.querySelector(".detail-no").textContent;
+        const no = parseInt(noStr);
+        tr.querySelector(".detail-no").textContent = no - 1;
+      });
+
+      parentTr.parentNode.removeChild(parentTr);
+    }
+  });
+
+  //TODO:送信処理
+}
+
+const getNextAll = element => {
+  const nextElements = [];
+  let nextElement = element.nextElementSibling;
+  while (nextElement) {
+    nextElements.push(nextElement);
+    nextElement = nextElement.nextElementSibling;
+  }
+  return nextElements;
+}
---
+++ Bpms/wwwroot/js/DailyAttendance/test.js
@@ -0,0 +1,21 @@
+﻿window.onload = () => {
+  document.querySelector("#detail-add-button").onclick = () => {
+    const numOfDetails = document.querySelectorAll("#detail-table > tbody > tr").length;
+    const addDetailIndex = numOfDetails;
+    const addDetailHtmlString =
+      `     <tr>
+            <td>${addDetailIndex + 1}</td>
+            <td>
+              <input type="text" class="hour-text" name="details[${addDetailIndex}].startHour" value="9" />時
+              <input type="text" class="minute-text" name="details[${addDetailIndex}].startMinute" value="00" />分～
+              <input type="text" class="hour-text" name="details[${addDetailIndex}].endHour" value="18" />時
+              <input type="text" class="minute-text" name="details[${addDetailIndex}].endMinute" value="00" />分
+            </td>
+            <td><textarea type="text" name="details[${addDetailIndex}].description"></textarea></td>
+            <td><button>削除</button></td>
+          </tr>`;
+
+    const detailTbody = document.querySelector("#detail-table > tbody");
+    detailTbody.insertAdjacentHTML('beforeend', addDetailHtmlString);
+  }
+}
